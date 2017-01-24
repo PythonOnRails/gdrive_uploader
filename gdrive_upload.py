9503bddd8eb6eb537730474020a3bceb00018ac7
Xiaobin <w@orx.me>
2017-01-24 16:06:13 +0800

    format

--- gdrive_upload.py
+++ gdrive_upload.py
@@ -16,7 +16,6 @@ from apiclient.errors import ResumableUploadError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 
-
 # Log only oauth2client errors
 logging.basicConfig(level="ERROR")
 
@@ -41,13 +40,13 @@ def file_ops(file_path):
 
 
 def create_token_file(token_file):
-# Run through the OAuth flow and retrieve credentials
+    # Run through the OAuth flow and retrieve credentials
     flow = OAuth2WebServerFlow(
         CLIENT_ID,
         CLIENT_SECRET,
         OAUTH_SCOPE,
         redirect_uri=REDIRECT_URI
-        )
+    )
     authorize_url = flow.step1_get_authorize_url()
     print('Go to the following link in your browser: ' + authorize_url)
     code = raw_input('Enter verification code: ').strip()
@@ -58,11 +57,11 @@ def create_token_file(token_file):
 
 
 def authorize(token_file, storage):
-# Get credentials
+    # Get credentials
     if storage is None:
         storage = Storage(token_file)
     credentials = storage.get()
-# Create an httplib2.Http object and authorize it with our credentials
+    # Create an httplib2.Http object and authorize it with our credentials
     http = httplib2.Http()
     credentials.refresh(http)
     http = credentials.authorize(http)
@@ -70,9 +69,9 @@ def authorize(token_file, storage):
 
 
 def upload_file(file_path, file_name, mime_type):
-# Create Google Drive service instance
+    # Create Google Drive service instance
     drive_service = build('drive', 'v2', http=http)
-# File body description
+    # File body description
     media_body = MediaFileUpload(file_path,
                                  mimetype=mime_type,
                                  resumable=True)
@@ -81,45 +80,48 @@ def upload_file(file_path, file_name, mime_type):
         'description': 'backup',
         'mimeType': mime_type,
     }
-# Permissions body description: anyone who has link can upload
-# Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
+    # Permissions body description: anyone who has link can upload
+    # Other permissions can be found at https://developers.google.com/drive/v2/reference/permissions
     permissions = {
         'role': 'reader',
         'type': 'anyone',
         'value': None,
         'withLink': True
     }
-# Insert a file
+    # Insert a file
     file = drive_service.files().insert(body=body, media_body=media_body).execute()
-# Insert new permissions
+    # Insert new permissions
     drive_service.permissions().insert(fileId=file['id'], body=permissions).execute()
-# Define file instance and get url for download
+    # Define file instance and get url for download
     file = drive_service.files().get(fileId=file['id']).execute()
     download_url = file.get('webContentLink')
     return download_url
 
+
 if __name__ == '__main__':
-# Check if file provied as argument and exists
+    # Check if file provied as argument and exists
     if len(sys.argv) != 2:
         print("One file should be provided as argument")
         sys.exit(1)
     else:
-# Path to the file to upload
+        # Path to the file to upload
         file_path = sys.argv[1]
     try:
-        with open(file_path) as f: pass
+        with open(file_path) as f:
+            pass
     except IOError as e:
         print(e)
         sys.exit(1)
-# Check if token file exists, if not create it by requesting authorization code
+    # Check if token file exists, if not create it by requesting authorization code
     try:
-        with open(token_file) as f: pass
+        with open(token_file) as f:
+            pass
     except IOError:
         http = authorize(token_file, create_token_file(token_file))
-# Authorize, get file parameters, upload file and print out result URL for download
+    # Authorize, get file parameters, upload file and print out result URL for download
     http = authorize(token_file, None)
     file_name, mime_type = file_ops(file_path)
-# Sometimes API fails to retrieve starting URI, we wrap it.
+    # Sometimes API fails to retrieve starting URI, we wrap it.
     try:
         print(upload_file(file_path, file_name, mime_type))
     except ResumableUploadError as e:
